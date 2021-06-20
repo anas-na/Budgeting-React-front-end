55f5cf25338ea8b93f6b0cd0ee044692d7cb97e8
anasnahil <nahil.anas1@gmail.com>
2021-06-20 12:35:44 -0400

    fixed routing error

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,16 @@
 import { Navbar, Nav } from "react-bootstrap";
 import logobudget from "../logobudget.png";
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar variant="light"  className="navBar">
       <Navbar.Brand href="/">
         <img src={logobudget} alt="logo" className="logo" />
       </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/transactions">All Transactions</Nav.Link>
-        <Nav.Link href="/transactions/new">New Transaction</Nav.Link>
+      <Nav className="mr-auto justify-content-between">
+        <Nav.Link className="m-5" as={Link} to="/transactions">All Transactions</Nav.Link>
+        <Nav.Link className="m-5" as={Link} to="/transactions/new">New Transaction</Nav.Link>
       </Nav>
     </Navbar>
   );
